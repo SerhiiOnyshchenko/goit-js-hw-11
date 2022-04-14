ced3a976483f6b756568dda759dd753ae1d5b434
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-04-14 11:23:56 +0300

    fixed something
    async/await and simplelightbox, scroll slowly

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 import API from './js/fetchCountries';
+import SimpleLightbox from 'simplelightbox';
 
 const refs = {
    input: document.querySelector('[name="searchQuery"]'),
@@ -8,29 +9,48 @@ const refs = {
    divImages: document.querySelector('.gallery'),
    loadMore: document.querySelector('.load-more'),
 };
-let countPage = 1;
 
-refs.form.addEventListener('click', evt => {
+let countPage = 1;
+let totalHits;
+const per_page = 20;
+let allCards;
+
+async function apiAndRender() {
+   const name = refs.input.value;
+   await API.fetchCountries(name, countPage, per_page).then(renderListImage).catch(onFetchError);
+   allCards = document.querySelectorAll('.gallery__item');
+}
+
+// first cards
+refs.form.addEventListener('click', async evt => {
    evt.preventDefault();
    countPage = 1;
    if (evt.target.type === 'submit') {
-      const name = refs.input.value;
       refs.divImages.innerHTML = '';
-      API.fetchCountries(name, countPage).then(renderListImage).catch(onFetchError);
-		refs.loadMore.classList.remove("selector")
-	}
+      await apiAndRender();
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      if (totalHits >= allCards.length + per_page) {
+         refs.loadMore.classList.remove('selector');
+      }
+      createGallerySimplelightbox();
+   }
 });
-
-refs.loadMore.addEventListener('click', evt => {
+// load more cards
+refs.loadMore.addEventListener('click', async evt => {
    evt.preventDefault();
    countPage += 1;
-   const name = refs.input.value;
-   API.fetchCountries(name, countPage).then(renderListImage).catch(onFetchError);
+   await apiAndRender();
+   if (totalHits === allCards.length) {
+      refs.loadMore.classList.add('selector');
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+   }
+	refreshGallerySimplelightbox();
+	scrollSlowly()
 });
 
 // render function
 function renderListImage(images) {
-   console.log(images);
+   totalHits = images.totalHits;
    if (!images.hits.length) {
       throw new Error();
    }
@@ -38,8 +58,8 @@ function renderListImage(images) {
       const markup = images.hits
          .map(image => {
             const { webformatURL, largeImageURL, tags, likes, comments, views, downloads } = image;
-            return `<div class="photo-card">
-								<img height="240" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            return `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card">
+								<img class="gallery__image" height="240" src="${webformatURL}" alt="${tags}" loading="lazy" />
 								<div class="info">
 									<p class="info-item">
 										<b>Likes</b> ${likes}
@@ -54,9 +74,10 @@ function renderListImage(images) {
 										<b>Downloads</b> ${downloads}
 									</p>
 								</div>
-							</div>`;
+							</div></a>`;
          })
          .join(' ');
+
       return (refs.divImages.innerHTML += markup);
    }
 }
@@ -66,4 +87,30 @@ function onFetchError() {
    Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
    );
+}
+
+// gallery simplelightbox
+function createGallerySimplelightbox() {
+   let gallery = new SimpleLightbox('.gallery a');
+   gallery.on('show.simplelightbox', function () {
+      // do something…
+   });
+}
+function refreshGallerySimplelightbox() {
+   let gallery = new SimpleLightbox('.gallery a');
+   gallery.refresh('show.simplelightbox', function () {
+      // do something…
+   });
+}
+
+// scroll slowly
+function scrollSlowly() {
+   const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+   window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+   });
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,12 @@
 const MY_KEY = '26722217-c4f85004f69e1760da5af6d6e';
 const API_URL = 'https://pixabay.com/api/';
-const per_page = 5;
 
-function fetchCountries(name, page) {
-   return fetch(
+
+async function fetchCountries(name, page, per_page) {
+   const response = await fetch(
       `${API_URL}?key=${MY_KEY}&q=${name}&image_type=photo$orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
-   ).then(response => {
-      if (!response.ok) {
-         throw new Error(response.status);
-      }
-      return response.json();
-   });
+   );
+   const cards = await response.json();
+   return cards;
 }
 export default { fetchCountries };
