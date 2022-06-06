d142774743b306510cddc448b52d1fd9d861ab82
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-06-06 16:35:48 +0300

    scroll more

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ import Notiflix from 'notiflix';
 import API from './js/fetchCountries';
 import SimpleLightbox from 'simplelightbox';
 import LoadMoreBtn from './js/lode-more-btn';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle';
 
 const refs = {
    input: document.querySelector('[name="searchQuery"]'),
@@ -11,9 +13,9 @@ const refs = {
 };
 const LoadeMoreBtn = new LoadMoreBtn({ selector: '.load-more' });
 let countPage = 1;
-let totalHits;
+let totalHits = 0;
 const per_page = 40;
-let allCards;
+let allCards = [];
 
 async function apiAndRender() {
    const name = refs.input.value;
@@ -49,6 +51,7 @@ refs.form.addEventListener('click', async evt => {
       // scrollSlowly();
    }
 });
+
 // load more cards
 LoadeMoreBtn.refs.button.addEventListener('click', loadeMore);
 
@@ -132,3 +135,21 @@ function scrollSlowly() {
       behavior: 'smooth',
    });
 }
+
+// scroll more
+window.addEventListener(
+   'scroll',
+   throttle(() => {
+      let contentHeight = refs.divImages.offsetHeight; // 1) высота блока контента вместе с границами
+      let yOffset = window.pageYOffset; // 2) текущее положение скролбара
+      let window_height = window.innerHeight; // 3) высота внутренней области окна документа
+      let y = yOffset + window_height;
+      // если пользователь достиг конца
+      if (y - 100 >= contentHeight) {
+         //загружаем новое содержимое в элемент
+         if (contentHeight !== 40 && totalHits !== allCards.length) {
+            loadeMore();
+         }
+      }
+   }, 500)
+);
